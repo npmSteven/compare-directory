c5c24fe9deaae2680daa5df239123b6aea83a9de
Steven Rafferty <mail@stevenrafferty.net>
2025-01-14 19:39:28 +0000

    inital work

---
+++ app.js
@@ -0,0 +1,43 @@
+const fs = require('fs/promises');
+
+async function checkFileExists(file) {
+  try {
+    await fs.access(file, fs.constants.F_OK)
+  } catch (error) {
+    return false
+  }
+  return true;
+}
+
+const sourcePath = process.argv[2];
+const comparePath = process.argv[3];
+
+async function init() {
+  try {
+    const [
+      doesSourcePathExist, doesComparePathExist
+    ] = await Promise.all([
+      checkFileExists(sourcePath),
+      checkFileExists(comparePath),
+    ]);
+    if (!doesSourcePathExist || !doesComparePathExist) {
+      throw new Error('One of the paths does not exist');
+    }
+    const [
+      sourceFiles,
+      compareFiles,
+    ] = await Promise.all([
+      fs.readdir(sourcePath),
+      fs.readdir(comparePath)
+    ]);
+    console.log(`Checking if there are files in the source`);
+    const notFoundFiles = compareFiles.find(cf => !sourceFiles.includes(cf));
+    console.log(notFoundFiles);
+
+  } catch (error) {
+    console.error('ERROR - init():', error);
+    process.exit(1);
+  }
+}
+
+init();
