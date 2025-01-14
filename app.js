d73537540abfbaf6d9792b85e5b06cf77e1f12c2
Steven Rafferty <mail@stevenrafferty.net>
2025-01-14 21:18:29 +0000

    Check for subfiles

--- app.js
+++ app.js
@@ -32,9 +32,34 @@ async function init() {
     ]);
     console.log(`Checking if there are files in the source`);
     console.log(`source files ${sourceFiles.length} compare files ${compareFiles.length}`)
-    const notFoundFiles = compareFiles.find(cf => !sourceFiles.includes(cf));
+    const notFoundFiles = [];
+    for (let i = 0; i < compareFiles.length; i++) {
+      const compareFile = compareFiles[i];
+      const sourceFile = sourceFiles.find(sf => sf === compareFile);
+      if (!sourceFile) {
+        notFoundFiles.push(sourceFile);
+        continue;
+      }
+      const [
+        sourceFileChildren,
+        compareFileChildren
+      ] = await Promise.all([
+        fs.readdir(`${sourcePath}/${sourceFile}`),
+        fs.readdir(`${comparePath}/${sourceFile}`)
+      ]);
+      const hasEveryFile = compareFileChildren.reduce((acc, item) => {
+        const sourceFileChild = sourceFileChildren.find(sf => sf === item);
+        if (!sourceFileChild) {
+          acc = false;
+          return acc;
+        }
+        return acc;
+      }, true);
+      if (!hasEveryFile) {
+        notFoundFiles.push(sourceFile);
+      }
+    }
     console.log(notFoundFiles);
-
   } catch (error) {
     console.error('ERROR - init():', error);
     process.exit(1);
